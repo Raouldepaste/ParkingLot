061691909a37d050e19264a54f4ff1e602578f24
Ivan Raoul <raoulivan81@gmail.com>
2025-11-10 08:35:23 +0200

    Created the entities

---
+++ src/main/java/org/example/parkinglot/entities/Car.java
@@ -0,0 +1,53 @@
+package org.example.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String parkingSpot;
+
+    private String licensePlace;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getLicensePlace() {
+        return licensePlace;
+    }
+
+    public void setLicensePlace(String licensePlace) {
+        this.licensePlace = licensePlace;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
