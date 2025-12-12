a234550a510d2e06c8034382d8b9ed23ad6ecb9e
Ivan Raoul <raoulivan81@gmail.com>
2025-12-12 10:33:54 +0200

    added Update and Delete buttons for Cars

--- src/main/java/org/example/parkinglot/ejb/CarsBean.java
+++ src/main/java/org/example/parkinglot/ejb/CarsBean.java
@@ -10,6 +10,7 @@ import org.example.parkinglot.entities.Car;
 import org.example.parkinglot.entities.User;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -56,4 +57,35 @@ public class CarsBean {
         entityManager.persist(car);
     }
 
+    public CarDto findById(Long carId) {
+        LOG.info("findById");
+        Car car = entityManager.find(Car.class, carId);
+        if (car == null) {
+            throw new RuntimeException("Car with id " + carId + " not found");
+        }
+        return new CarDto(car.getId(), car.getLicensePlate(), car.getParkingSpot(), car.getOwner().getUsername());
+    }
+
+    public void updateCar(Long carId, String licensePlate, String parkingSpot, Long userId) {
+        LOG.info("updateCar");
+
+        Car car = entityManager.find(Car.class, carId);
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        User oldUser = car.getOwner();
+        oldUser.getCars().remove(car);
+
+        User user = entityManager.find(User.class, userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+    }
+
+    public void deleteCarsByIds(Collection<Long> carIds) {
+        LOG.info("deleteCarsByIds");
+        for (Long carId : carIds) {
+            Car car = entityManager.find(Car.class, carId);
+            entityManager.remove(car);
+        }
+    }
 }
